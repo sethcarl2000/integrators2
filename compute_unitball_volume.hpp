5f23c7abbcbf2a10d83ab596e2ea0c673d38a796
sethcarl2000 <sethhall@jlab.org>
2025-10-01 17:58:20 -0400

    Moved unitball volume computation to 'compute_unitball_volume.hpp/cpp'

---
+++ compute_unitball_volume.hpp
@@ -0,0 +1,6 @@
+#ifndef compute_unitball_volume_H
+#define compute_unitball_volume_H 
+
+void compute_unitball_volume(const int n_dim_min=2, const int n_dim_max=15, const char* path_png="nball_volume.png", const long unsigned int integration_pts=1e7);  
+
+#endif 
--- integrate.cpp
+++ integrate.cpp
@@ -1,4 +1,5 @@
 #include <MontecarloIntegrate.hpp> 
+#include "compute_unitball_volume.hpp"
 #include <cmath> 
 #include <iostream>
 #include <TGraph.h> 
@@ -12,55 +13,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {   
-    const int n_dim_min = 2; 
-    const int n_dim_max = 15; 
-
-    const long unsigned int integrations_pts = 1e7; 
-
-    vector<double> pts_dimension, pts_vol;
-
-    for (int dim = n_dim_min; dim<=n_dim_max; dim++) {
-
-        //create our bounding box as the 'unit' cube (with s=2)
-        vector<IntegrationBound_t> bounds;
-        for (int d=0; d<dim; d++) bounds.push_back({-1., 1.}); 
-        
-        //define our fcn, which returns 'true' when inside the unit sphere
-        auto is_in_ball = [dim](const double* X) 
-        {
-            double val=0.; 
-            for (int i=0; i<dim; i++) val += X[i]*X[i]; 
-            return val < 1. ? true : false; 
-        };
-        
-        auto volume = MontecarloIntegrate(integrations_pts, bounds, is_in_ball);
-
-        pts_dimension.push_back((double)dim); 
-        pts_vol      .push_back(volume.val); 
-    
-        printf(" %2i/%i - vol: %.6f +/- %.6f\n", dim, n_dim_max, volume.val, volume.error);
-        cout << flush; 
-    }
-
-    auto graph = new TGraph(pts_dimension.size(), pts_dimension.data(), pts_vol.data()); 
-
-    auto tf1 = new TF1("volume", "pow(TMath::Pi(), x/2.)/ROOT::Math::tgamma(x/2. + 1.)", n_dim_min, n_dim_max); 
-
-    auto canv = new TCanvas; 
-
-    //gPad->SetLogy(1); 
-    
-    graph->SetTitle("Volume of unit (R=1) N-ball;dimension;volume"); 
-
-    graph->SetLineStyle(kSolid); 
-    graph->SetMarkerStyle(kOpenCircle); 
-    graph->Draw();
-
-    tf1->SetLineWidth(2);
-    tf1->SetLineStyle(kDashed); 
-    tf1->Draw("SAME"); 
-
-    canv->SaveAs("nball_volume.png"); 
+    compute_unitball_volume(2, 15, "nball_volume.png"); 
     
     return 0; 
 }
