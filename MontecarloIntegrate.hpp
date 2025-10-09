8cfc9e31850ef0574bc84914f6f8a3e673fe060a
sethcarl2000 <sethhall@jlab.org>
2025-10-09 13:05:08 -0400

    Move definition of 'IntegrationBound_t' struct

--- MontecarloIntegrate.hpp
+++ MontecarloIntegrate.hpp
@@ -10,11 +10,6 @@
 
 // A generalized monte-carlo integration tool 
 
-// tells you what your integration bounds are
-struct IntegrationBound_t {
-    double xmin{-1.}, xmax{1.};
-};
-
 ValueWithError_t<double> MontecarloIntegrate(
     const unsigned long int n_pts,                  //number of points to use in the integration 
     const std::vector<IntegrationBound_t> bounds,   //number of dimensions is given by the number of bounds given.  
